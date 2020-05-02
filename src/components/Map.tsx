f1af41713cd0fc75d7390db8d1ca4830ae5b3554
Maximilian Zirps <maximilian.zirps@gmail.com>
2020-05-02 21:16:16 +0200

    draw u6 circles

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,246 @@
 import React, { FunctionComponent } from "react";
-import map from "./underground.svg";
 
-const Map: FunctionComponent = () => (
-  <div id="map-wrapper">
-    <img src={map} alt="underground map" />
-  </div>
-);
+const Map: FunctionComponent = () => {
+  const siebenhirtenactive = true;
+
+  return (
+    <div id="map-wrapper">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 100 1200 800">
+        <polyline
+          points="612,744 535,666 535,440 850,125"
+          style={{ fill: "none", stroke: "red", strokeWidth: "15" }}
+          id="u1"
+        />
+        <polyline
+          points="1047,312 991,365 928,335 815,455 685,332 545,332 450,418 450,481 495,520 536,520"
+          style={{ fill: "none", stroke: "violet", strokeWidth: "15" }}
+          id="u2"
+        />
+        <polyline
+          points="230,338 350,450 650,450 765,565 765,650"
+          style={{ fill: "none", stroke: "orange", strokeWidth: "15" }}
+          id="u3"
+        />
+        <polyline
+          points="140,540 575,540 617,500 617,400 485,237 485,164"
+          style={{ fill: "none", stroke: "green", strokeWidth: "15" }}
+          id="u4"
+        />
+        <polyline
+          points="210,805 350,675 350,318 450,219 515, 219 613,120"
+          style={{ fill: "none", stroke: "brown", strokeWidth: "15" }}
+          id="u6"
+        />
+        <circle
+          id="siebenhirten"
+          cx="215"
+          cy="800"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill={siebenhirtenactive ? "brown" : "white"}
+        />
+        <circle
+          id="perfektastrasse"
+          cx="240"
+          cy="775"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="erlaaerstrasse"
+          cx="270"
+          cy="750"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="alterlaa"
+          cx="290"
+          cy="730"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="amschoepfwerk"
+          cx="310"
+          cy="710"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="tschertegasse"
+          cx="330"
+          cy="695"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="bahnhofmeidling"
+          cx="350"
+          cy="675"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="laengenfeldgasse"
+          cx="350"
+          cy="540"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="gumpendorferstrasse"
+          cx="350"
+          cy="495"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="westbahnhof"
+          cx="350"
+          cy="448"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="burggassestadthalle"
+          cx="350"
+          cy="410"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="thalistrasse"
+          cx="350"
+          cy="375"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="josefstaedterstrasse"
+          cx="350"
+          cy="340"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="alserstrasse"
+          cx="370"
+          cy="300"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="michelbeuern-akh"
+          cx="390"
+          cy="280"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="wahringerstrasse-volksoper"
+          cx="410"
+          cy="260"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="nussdorferstrasse"
+          cx="430"
+          cy="240"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="spittelau"
+          cx="485"
+          cy="220"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="jaegerstrasse"
+          cx="510"
+          cy="219"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="dresdnerstrasse"
+          cx="530"
+          cy="205"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="handelskai"
+          cx="550"
+          cy="185"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="neuedonau"
+          cx="575"
+          cy="160"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+        <circle
+          id="floridsdorf"
+          cx="605"
+          cy="125"
+          r="10"
+          stroke="black"
+          strokeWidth="3"
+          fill="white"
+        />
+      </svg>
+    </div>
+  );
+};
 
 export default Map;
