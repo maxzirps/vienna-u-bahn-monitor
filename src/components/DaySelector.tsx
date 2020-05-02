336ba21518d7c66c9dcfe04d9ddae71bcb98b174
Maximilian Zirps <maximilian.zirps@gmail.com>
2020-05-02 11:52:49 +0200

    fix radiobutton styling

--- src/components/DaySelector.tsx
+++ src/components/DaySelector.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Radio } from "semantic-ui-react";
+import { Radio, Form } from "semantic-ui-react";
 
 export default class DaySelector extends React.Component<
   {},
@@ -15,29 +15,35 @@ export default class DaySelector extends React.Component<
   render(): any {
     const { selected } = this.state;
     return (
-      <>
-        <Radio
-          label="Mon-Fri"
-          name="radioGroup"
-          value="this"
-          checked={selected === "this"}
-          onChange={() => this.setState({ selected: "this" })}
-        />
-        <Radio
-          label="Sat"
-          name="radioGroup"
-          value="that"
-          checked={selected === "that"}
-          onChange={() => this.setState({ selected: "that" })}
-        />
-        <Radio
-          label="Sun"
-          name="radioGroup"
-          value="that"
-          checked={selected === "that"}
-          onChange={() => this.setState({ selected: "that" })}
-        />
-      </>
+      <Form>
+        <Form.Field>
+          <Radio
+            label="Mon-Fri"
+            name="radioGroup"
+            value="weekday"
+            checked={selected === "weekday"}
+            onChange={(): void => this.setState({ selected: "weekday" })}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Radio
+            label="Sat"
+            name="radioGroup"
+            value="sat"
+            checked={selected === "sat"}
+            onChange={(): void => this.setState({ selected: "sat" })}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Radio
+            label="Sun"
+            name="radioGroup"
+            value="sun"
+            checked={selected === "sun"}
+            onChange={(): void => this.setState({ selected: "sun" })}
+          />
+        </Form.Field>
+      </Form>
     );
   }
 }
